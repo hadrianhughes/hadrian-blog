0bfdf07b4e96c03d3c4c2448c4f7682a1963bcf6
Hadrian Hughes <h.hughes@amaze.com>
2018-01-25 20:43:12 +0000

    Remove content module

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const app = express();
 const path = require('path');
 
 const render = require('./lib/render');
-const { getPostByUID } = require('./lib/content');
 const log = require('./lib/log');
 
 const PORT = process.env.PORT || 3000;
@@ -13,13 +12,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/post/:uid', (req, res) => {
-  getPostByUID(req.params.uid).then(data => {
-    res.send(render(req.url, data));
-  });
+  res.status(200);
+  res.end();
+  log.info({ route: 'post', status: '200', params: req.params }, 'Responded with 200');
 });
 
 app.listen(PORT, (err) => {
   if (err) throw err;
 
-  log.info(`Listening on port ${PORT}...`);
+  log.info({ port: PORT }, 'Started listening for requests');
 });
