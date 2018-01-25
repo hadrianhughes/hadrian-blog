bdefb0864e92d3371c477faf3b8e0245ed5ad5cf
Hadrian Hughes <h.hughes@amaze.com>
2018-01-25 22:24:34 +0000

    Set up routes with mock data

--- server/index.js
+++ server/index.js
@@ -11,10 +11,28 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../build')));
 
+app.get('/', (req, res) => {
+  res.status(200);
+  res.send(render('/', require('../../__mock-data__/post-list.json')));
+  log.info({ route: '/', status: 200 }, 'Responded with 200');
+});
+
 app.get('/post/:uid', (req, res) => {
   res.status(200);
-  res.end();
-  log.info({ route: 'post', status: '200', params: req.params }, 'Responded with 200');
+  res.send(render(req.url, require('../../__mock-data__/post.json')));
+  log.info({ route: 'post', status: 200, params: req.params }, 'Responded with 200');
+});
+
+app.get('/about', (req, res) => {
+  res.status(200);
+  res.send(render('/about', require('../../__mock-data__/about.json')));
+  log.info({ route: 'about', status: 200 }, 'Responded with 200');
+});
+
+app.get('/search/:terms', (req, res) => {
+  res.status(200);
+  res.send(render(req.url, require('../../__mock-data__/post-list.json')));
+  log.info({ route: 'search', status: 200, req.params }, 'Responded with 200');
 });
 
 app.listen(PORT, (err) => {
